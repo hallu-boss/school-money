dea08a30381775e28f6fb7c7f1f286a1e9c07b2e
Paweł Hase <pawelhase@gmail.com>
2025-10-10 22:50:19 +0200

    Formated files

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,19 +1,19 @@
-import React from "react";
-import { Box, CircularProgress } from "@mui/material";
+import React from 'react';
+import { Box, CircularProgress } from '@mui/material';
 
 const Loading = () => {
-    return (
-        <Box
-            sx={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                height: '50vh',
-            }}
-        >
-            <CircularProgress />
-        </Box>
-    )
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '50vh',
+      }}
+    >
+      <CircularProgress />
+    </Box>
+  );
 };
 
 export default Loading;
