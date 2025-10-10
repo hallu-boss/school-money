2254b5361a359b0b9d8685432d170134de60f269
Paweł Hase <pawelhase@gmail.com>
2025-10-10 21:57:31 +0200

    Added loading

---
+++ app/loading.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Box, CircularProgress } from "@mui/material";
+
+const Loading = () => {
+    return (
+        <Box
+            sx={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '50vh',
+            }}
+        >
+            <CircularProgress />
+        </Box>
+    )
+};
+
+export default Loading;
