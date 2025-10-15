585ec2ffb2801785ada445bd8fb5e4c967cf92f0
Erneo97 <michalkaniewski1997@gmail.com>
2025-10-15 22:37:48 +0200

    Dodanie Widoku dziecka

---
+++ app/components/profile/ChildCard.tsx
@@ -0,0 +1,52 @@
+'use client';
+
+import { Avatar, Box, Paper, Stack, Typography } from '@mui/material';
+
+import { Child } from '@prisma/client';
+
+type ChildCardProps = {
+  user?: Child;
+};
+
+const ChildCard = ({ user }: ChildCardProps) => {
+  if (!user) {
+    return (
+      <Paper sx={{ bgcolor: 'grey.80', p: 2, textAlign: 'center' }}>
+        Brak informacji o użytkowniku
+      </Paper>
+    );
+  }
+
+  return (
+    <Paper elevation={5}
+      sx={{
+        borderRadius: 10,
+        bgcolor: 'grey.80',
+        m: 2,
+        p: 2,
+      }}
+    >
+      <Stack direction="row" spacing={1} alignItems="flex-start">
+        <Box>
+          <Avatar
+            src={user.avatarUrl || undefined}
+            alt={user.name || 'Użytkownik'}
+            sx={{
+              width: 120,
+              height: 120,
+              fontSize: 50,
+              bgcolor: 'primary.main',
+            }}
+          />
+        </Box>
+        <Box textAlign={'left'}>
+          <Typography variant='h3'>{user.name}</Typography>
+          {/* TODO: dodać klasę ucznia*/}
+          <Typography variant='h3' color='#01579b'>IIIC</Typography>
+        </Box>
+      </Stack>
+    </Paper>
+  );
+};
+
+export { ChildCard };
--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,22 @@ import { Container, Paper, Typography } from '@mui/material';
 import { redirect } from 'next/navigation';
 import { SignOut } from './components/SignOut';
 import { UserInformation } from './components/profile/UserInformation';
+import { Child } from '@prisma/client';
+import { ChildCard } from './components/profile/ChildCard';
 
 export default async function Home() {
   const session = await auth();
   if (!session) redirect('/sign-in');
   console.log(session.user);
+
+  
+  const dziecko1: Child = {
+    id: "2342sd",
+    name: "Johan Muller",
+    avatarUrl: "https://www.humanium.org/en/wp-content/uploads/2025/05/shutterstock_2267902633-1024x683.jpg",
+    userId: "1",
+    createdAt:  new Date() 
+  };
 
   return (
     <Container maxWidth="sm" sx={{ mt: 8 }}>
@@ -23,6 +34,7 @@ export default async function Home() {
         }}
       >
         <UserInformation user={session.user} />
+        <ChildCard user={dziecko1}/>
 
         <Typography variant="body1" color="text.secondary">
           Signed in as:
