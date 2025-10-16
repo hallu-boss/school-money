a3ebe1cd686051be64968bc3dbacb53387571a75
Erneo97 <michalkaniewski1997@gmail.com>
2025-10-16 11:16:26 +0200

    Utworzenie listy dzieci Każde okienka z dzieckiem można kliknąć

--- app/components/profile/ChildCard.tsx
+++ app/components/profile/ChildCard.tsx
@@ -1,14 +1,15 @@
 'use client';
 
 import { Avatar, Box, Paper, Stack, Typography } from '@mui/material';
-
 import { Child } from '@prisma/client';
+import { Card, CardActionArea, CardContent } from '@mui/material';
 
 type ChildCardProps = {
   user?: Child;
+  onClick?: () => void;
 };
 
-const ChildCard = ({ user }: ChildCardProps) => {
+const ChildCard = ({ user, onClick }: ChildCardProps) => {
   if (!user) {
     return (
       <Paper sx={{ bgcolor: 'grey.80', p: 2, textAlign: 'center' }}>
@@ -18,37 +19,40 @@ const ChildCard = ({ user }: ChildCardProps) => {
   }
 
   return (
-    <Paper
+    <Card
       elevation={5}
       sx={{
-        borderRadius: 10,
+        borderRadius: 5,
         bgcolor: 'grey.80',
         m: 2,
-        p: 2,
       }}
     >
-      <Stack direction="row" spacing={1} alignItems="flex-start">
-        <Box>
-          <Avatar
-            src={user.avatarUrl || undefined}
-            alt={user.name || 'Użytkownik'}
-            sx={{
-              width: 120,
-              height: 120,
-              fontSize: 50,
-              bgcolor: 'primary.main',
-            }}
-          />
-        </Box>
-        <Box textAlign={'left'}>
-          <Typography variant="h3">{user.name}</Typography>
-          {/* TODO: dodać klasę ucznia*/}
-          <Typography variant="h3" color="#01579b">
-            IIIC
-          </Typography>
-        </Box>
-      </Stack>
-    </Paper>
+      <CardActionArea onClick={onClick}>
+        <CardContent>
+          <Stack direction="row" spacing={1} alignItems="flex-start">
+            <Box>
+              <Avatar
+                src={user.avatarUrl || undefined}
+                alt={user.name || 'Użytkownik'}
+                sx={{
+                  width: 120,
+                  height: 120,
+                  fontSize: 50,
+                  bgcolor: 'primary.main',
+                }}
+              />
+            </Box>
+            <Box textAlign={'left'}>
+              <Typography variant="h3">{user.name}</Typography>
+              {/* TODO: dodać klasę ucznia*/}
+              <Typography variant="h3" color="#01579b">
+                IIIC
+              </Typography>
+            </Box>
+          </Stack>
+        </CardContent>
+      </CardActionArea>
+    </Card>
   );
 };
 
--- app/page.tsx
+++ app/page.tsx
@@ -6,22 +6,34 @@ import { UserInformation } from './components/profile/UserInformation';
 import { Child } from '@prisma/client';
 import { ChildCard } from './components/profile/ChildCard';
 
+import { Box } from '@mui/material';
+
 export default async function Home() {
   const session = await auth();
   if (!session) redirect('/sign-in');
   console.log(session.user);
 
-  const dziecko1: Child = {
-    id: '2342sd',
-    name: 'Johan Muller',
-    avatarUrl:
-      'https://www.humanium.org/en/wp-content/uploads/2025/05/shutterstock_2267902633-1024x683.jpg',
-    userId: '1',
-    createdAt: new Date(),
-  };
+  const childrenList: Child[] = [
+    {
+      id: '2342sd',
+      name: 'Johan Muller',
+      avatarUrl:
+        'https://www.humanium.org/en/wp-content/uploads/2025/05/shutterstock_2267902633-1024x683.jpg',
+      userId: '1',
+      createdAt: new Date(),
+    },
+    {
+      id: '2342sd',
+      name: 'Elie Muller',
+      avatarUrl:
+        'http://ocdn.eu/pulscms-transforms/1/A2rk9kpTURBXy85NWNlYmQwNTY3NTZjYjZkMWJmY2Q5ZGIyOWFjMjUzMS5qcGeSlQMCzQEXzQu3zQaYkwXNBLDNAqTeAAGhMAE',
+      userId: '1',
+      createdAt: new Date(),
+    },
+  ];
 
   return (
-    <Container maxWidth="sm" sx={{ mt: 8 }}>
+    <Container sx={{ mt: 8 }}>
       <Typography variant="h4" component="h1" align="center" gutterBottom>
         SchoolMoney Project – HOME page
       </Typography>
@@ -34,7 +46,21 @@ export default async function Home() {
         }}
       >
         <UserInformation user={session.user} />
-        <ChildCard user={dziecko1} />
+        <Typography variant="h3" fontWeight="bold" margin={2}>
+          Twoje dzieci
+        </Typography>
+
+        <Box
+          sx={{
+            display: 'grid',
+            gridTemplateColumns: 'repeat(2, 1fr)',
+            gap: 2,
+          }}
+        >
+          {childrenList.map((user) => (
+            <ChildCard key={user.id} user={user} />
+          ))}
+        </Box>
 
         <Typography variant="body1" color="text.secondary">
           Signed in as:
