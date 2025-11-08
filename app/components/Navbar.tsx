e852ec2b7d95b75bee261b4f51b81de6d08a50bf
Byk0X <maciej.lencewicz@gmail.com>
2025-11-08 13:45:29 +0100

    Add active oage styling and flex spacer for sign out button

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,24 +1,38 @@
 'use client';
 
-import { AppBar, Button, Toolbar, Typography } from '@mui/material';
+import { AppBar, Box, Button, Toolbar, Typography } from '@mui/material';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { SignOut } from './SignOut';
 
 export default function Navbar() {
   const pathname = usePathname();
+
+  const pages = [
+    { name: 'Home', href: '/' },
+    { name: 'Profil', href: '/profile' },
+  ];
   return (
     <AppBar position="static" color="primary">
-      <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
-        <Typography variant="h6">SchoolMoney</Typography>
-        <Button component={Link} href="/" color="inherit">
-          Home
-        </Button>
-
-        <Button component={Link} href="/profile" color="inherit">
-          Profil
-        </Button>
-
+      <Toolbar disableGutters sx={{ px: 2 }}>
+        <Typography variant="h6" sx={{ mr: 2 }}>
+          SchoolMoney
+        </Typography>
+        {pages.map((page) => (
+          <Button
+            key={page.href}
+            component={Link}
+            href={page.href}
+            sx={{
+              color: pathname === page.href ? '#FFD700' : '#ffffff',
+              fontWeight: pathname === page.href ? 'bold' : 'normal',
+              textDecoration: pathname === page.href ? 'underline' : 'none',
+            }}
+          >
+            {page.name}
+          </Button>
+        ))}
+        <Box sx={{ flexGrow: 1 }}></Box>
         <SignOut />
       </Toolbar>
     </AppBar>
