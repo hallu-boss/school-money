0eb7480a14dd5bbd209ff1a96bfa57fc34175b5b
Byk0X <maciej.lencewicz@gmail.com>
2025-11-14 00:16:59 +0100

    Complete dialog for joining class

--- app/class/JoinClassDialog.tsx
+++ app/class/JoinClassDialog.tsx
@@ -1,4 +1,13 @@
-import { Dialog, DialogContent } from '@mui/material';
+import {
+  Box,
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  TextField,
+  Typography,
+  Button,
+  Stack,
+} from '@mui/material';
 
 type JoinClassDialogProps = {
   open: boolean;
@@ -6,13 +15,50 @@ type JoinClassDialogProps = {
 };
 
 export const JoinClassDialog = ({ open, onClose }: JoinClassDialogProps) => {
-  const handleClose = () => {
-    onClose();
-  };
-
   return (
-    <Dialog open={open} onClose={handleClose}>
-      <DialogContent>Dołącz do klasy</DialogContent>
+    <Dialog
+      open={open}
+      onClose={onClose}
+      maxWidth="xs"
+      fullWidth
+      PaperProps={{
+        sx: { borderRadius: 3, p: 1 },
+      }}
+    >
+      <DialogTitle
+        sx={{
+          borderBottom: 1,
+          borderColor: 'divider',
+          pb: 2,
+        }}
+      >
+        <Typography variant="h5" fontWeight="600">
+          Dołącz do klasy
+        </Typography>
+      </DialogTitle>
+
+      <DialogContent sx={{ pt: 3 }}>
+        <Stack spacing={3}>
+          <Box>
+            <TextField
+              sx={{ mt: 2 }}
+              label="Kod klasy"
+              placeholder="Wpisz kod..."
+              fullWidth
+              variant="outlined"
+            />
+          </Box>
+
+          <Stack direction="row" justifyContent="flex-end" spacing={2}>
+            <Button onClick={onClose} color="inherit">
+              Anuluj
+            </Button>
+            <Button variant="contained" color="primary">
+              Dołącz
+            </Button>
+          </Stack>
+        </Stack>
+      </DialogContent>
     </Dialog>
   );
 };
