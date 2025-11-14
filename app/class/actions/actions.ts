ceb2021b52f418ea611d3027c95b2c2fc25c542e
Byk0X <maciej.lencewicz@gmail.com>
2025-11-14 02:17:26 +0100

    Add function to create class

--- app/class/actions/actions.ts
+++ app/class/actions/actions.ts
@@ -1,1 +1,27 @@
-export async function getUserClasses() {}
+'use server';
+
+import { auth } from '@/lib/auth';
+import db from '@/lib/db';
+import { success } from 'zod';
+
+export const getSchools = async () => {
+  const session = await auth();
+  if (!session?.user?.id) {
+    throw new Error('Unauthorized');
+  }
+
+  const data = await db.school.findMany({
+    orderBy: { name: 'asc' },
+  });
+
+  return data;
+};
+
+export const createClass = async (payload: FormData) => {
+  const session = await auth();
+  if (!session?.user?.id) {
+    throw new Error('Unauthorized');
+  }
+  console.log(payload);
+  return { success: true, message: 'Successfully added class' };
+};
