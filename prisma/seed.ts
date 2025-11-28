51269c573a31df1f93372662fc73fb32ae13619d
Paweł Hase <pawelhase@gmail.com>
2025-11-28 16:04:01 +0100

    seed update

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,11 +4,13 @@ import argon2 from 'argon2';
 const prisma = new PrismaClient();
 
 async function clearDB() {
+  await prisma.payment.deleteMany();
   await prisma.transaction.deleteMany();
   await prisma.invoice.deleteMany();
-  await prisma.collectionAccount.deleteMany();
+  await prisma.bankAccount.deleteMany();
   await prisma.collection.deleteMany();
   await prisma.child.deleteMany();
+  await prisma.collectionParticipant.deleteMany();
   await prisma.classMembership.deleteMany();
   await prisma.class.deleteMany();
   await prisma.school.deleteMany();
@@ -20,26 +22,48 @@ async function clearDB() {
 }
 
 const hashedPassword = await argon2.hash('password123');
-let ibanSuffix = 0;
+
+type CreateBankAccountInput =
+  | { userId: string; collectionId?: never; iban?: string; balance?: number }
+  | { collectionId: string; userId?: never; iban?: string; balance?: number };
+
+export async function createBankAccount(data: CreateBankAccountInput) {
+  return prisma.bankAccount.create({
+    data: {
+      iban: data.iban ?? generateIban(),
+      balance: data.balance ?? 0,
+      userId: data.userId,
+      collectionId: data.collectionId,
+    },
+  });
+}
+
+function generateIban() {
+  const rand = Math.random().toString().slice(2, 18); // losowy 16-cyfrowy fragment
+  return `PL${rand.padStart(26, "0")}`;
+}
 
 async function createMockUser(name: string, email: string, image: string | null = null) {
-  ibanSuffix += 1;
-  return await prisma.user.create({
+  
+  const user = await prisma.user.create({
     data: {
       name,
       email,
       password: hashedPassword,
       image,
-      iban: `PL60102010260000010203045${ibanSuffix.toString().padStart(2, '0')}`,
     },
   });
+
+  await createBankAccount({ userId: user.id });
+
+  return user;
 }
 
 async function createMockChild(name: string, parentId: string, avatarUrl: string | null = null) {
   return await prisma.child.create({
     data: {
       name,
-      userId: parentId,
+      parentId: parentId,
       avatarUrl,
     },
   });
@@ -51,39 +75,6 @@ async function createMockSchool(name: string) {
       name,
     },
   });
-}
-
-async function createMockCollection(
-  title: string,
-  classId: string,
-  authorId: string,
-  amountPerChild: number,
-  coverUrl?: string | null,
-  description?: string | null,
-) {
-  ibanSuffix += 1;
-
-  const collection = await prisma.collection.create({
-    data: {
-      title,
-      description,
-      coverUrl,
-      startAt: new Date(), // start teraz
-      endAt: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), // +7 dni
-      classId,
-      authorId,
-      amountPerChild,
-      isClosed: false,
-      account: {
-        create: {
-          iban: `PL66109010140000010203045${ibanSuffix.toString().padStart(2, '0')}`,
-          balance: 0,
-        },
-      },
-    },
-  });
-
-  return collection;
 }
 
 async function createMockClassWithMembers(
@@ -146,59 +137,76 @@ async function createMockClassWithMembers(
   return cl;
 }
 
+async function createMockCollection(
+  title: string,
+  classId: string,
+  authorId: string,
+  amountPerChild: number,
+  coverUrl?: string | null,
+  description?: string | null,
+) {
+  const collection = await prisma.collection.create({
+    data: {
+      title,
+      description,
+      coverUrl,
+      startAt: new Date(), // start teraz
+      endAt: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), // +7 dni
+      classId,
+      authorId,
+      amountPerChild,
+    },
+  });
+
+  await createBankAccount({ collectionId: collection.id })
+
+  return collection;
+}
+
 async function main() {
   console.log('Czyszczenie istniejących danych...');
   await clearDB();
 
   console.log('Tworzenie użytkowników...');
-  const u_jan_kowalski = await createMockUser('Jan Kowalski', 'jan.kowalski@example.com');
-  const u_anna_nowak = await createMockUser('Anna Nowak', 'anna.nowak@example.com');
-  const u_piotr_wisniewski = await createMockUser(
-    'Piotr Wiśniewski',
-    'piotr.wisniewski@example.com',
-  );
-  const u_maria_lewandowska = await createMockUser(
-    'Maria Lewandowska',
-    'maria.lewandowska@example.com',
-  );
-  const u_krzysztof_wojcik = await createMockUser(
-    'Krzysztof Wójcik',
-    'krzysztof.wojcik@example.com',
-  );
+  const u_jan_kowalski      = await createMockUser('Jan Kowalski', 'jan.kowalski@example.com');
+  const u_anna_nowak        = await createMockUser('Anna Nowak', 'anna.nowak@example.com');
+  const u_piotr_wisniewski  = await createMockUser('Piotr Wiśniewski', 'piotr.wisniewski@example.com');
+  const u_maria_lewandowska = await createMockUser('Maria Lewandowska', 'maria.lewandowska@example.com');
+  const u_krzysztof_wojcik  = await createMockUser('Krzysztof Wójcik', 'krzysztof.wojcik@example.com');
 
   console.log('Tworzenie dzieci...');
-  const d_kasia_kowalska = await createMockChild('Kasia Kowalska', u_jan_kowalski.id);
-  const d_michal_nowak = await createMockChild('Michał Nowak', u_anna_nowak.id);
-  const d_zuzia_wisniewska = await createMockChild('Zuzia Wiśniewska', u_piotr_wisniewski.id);
-  const d_aleksander_lewandowski = await createMockChild(
-    'Aleksander Lewandowski',
-    u_maria_lewandowska.id,
-  );
-  const d_julia_wójcik = await createMockChild('Julia Wójcik', u_krzysztof_wojcik.id);
+  const d_kasia_kowalska         = await createMockChild('Kasia Kowalska', u_jan_kowalski.id);
+  const d_michal_nowak           = await createMockChild('Michał Nowak', u_anna_nowak.id);
+  const d_zuzia_wisniewska       = await createMockChild('Zuzia Wiśniewska', u_piotr_wisniewski.id);
+  const d_aleksander_lewandowski = await createMockChild( 'Aleksander Lewandowski', u_maria_lewandowska.id, );
+  const d_julia_wójcik           = await createMockChild('Julia Wójcik', u_krzysztof_wojcik.id);
 
   console.log('Tworzenie szkół...');
-  const school1 = await createMockSchool(
-    'Szkoła Podstawowa nr 1 im. Marii Skłodowskiej-Curie w Warszawie',
-  );
-  await createMockSchool('Szkoła Podstawowa nr 24 z Oddziałami Integracyjnymi w Krakowie');
-  await createMockSchool('Szkoła Podstawowa nr 5 im. Henryka Sienkiewicza w Poznaniu');
-  await createMockSchool('Szkoła Podstawowa nr 12 z Oddziałami Sportowymi we Wrocławiu');
-  await createMockSchool('Szkoła Podstawowa nr 8 im. Jana Pawła II w Gdańsku');
+  const school_p_nr1  = await createMockSchool( 'Szkoła Podstawowa nr 1 im. Marii Skłodowskiej-Curie w Warszawie', );
+  const school_p_nr24 = await createMockSchool('Szkoła Podstawowa nr 24 z Oddziałami Integracyjnymi w Krakowie');
+  const school_p_nr5  = await createMockSchool('Szkoła Podstawowa nr 5 im. Henryka Sienkiewicza w Poznaniu');
+  const school_p_nr12 = await createMockSchool('Szkoła Podstawowa nr 12 z Oddziałami Sportowymi we Wrocławiu');
+  const school_p_nr8  = await createMockSchool('Szkoła Podstawowa nr 8 im. Jana Pawła II w Gdańsku');
 
   console.log('Tworzenie klasy IA i dodawanie wszystkich userów + dzieci...');
 
-  const school1Class = await createMockClassWithMembers('IA', school1.id, u_jan_kowalski.id, [
-    { userId: u_anna_nowak.id, children: [d_michal_nowak] },
-    { userId: u_piotr_wisniewski.id, children: [d_zuzia_wisniewska] },
-    { userId: u_maria_lewandowska.id, children: [d_aleksander_lewandowski] },
-    { userId: u_krzysztof_wojcik.id, children: [d_julia_wójcik] },
-    { userId: u_jan_kowalski.id, children: [d_kasia_kowalska] },
-  ]);
+  const class_p_nr1_IA = await createMockClassWithMembers(
+    'IA', 
+    school_p_nr1.id, 
+    u_jan_kowalski.id, 
+    [
+      { userId: u_anna_nowak.id, children: [d_michal_nowak] },
+      { userId: u_piotr_wisniewski.id, children: [d_zuzia_wisniewska] },
+      { userId: u_maria_lewandowska.id, children: [d_aleksander_lewandowski] },
+      { userId: u_krzysztof_wojcik.id, children: [d_julia_wójcik] },
+      { userId: u_jan_kowalski.id, children: [d_kasia_kowalska] },
+    ]
+);
 
   console.log('Tworzenie przykładowej zbiórki...');
   await createMockCollection(
     'Składka na wycieczkę do Torunia',
-    school1Class.id,
+    class_p_nr1_IA.id,
     u_jan_kowalski.id,
     50.0,
     'https://gfx.chillizet.pl/var/g3-chillizet-2/storage/images/podroze/torun-na-weekend-10-najwiekszych-atrakcji-tego-miasta/21993551-1-pol-PL/To-najstarsze-miasto-w-Polsce.-Stad-pochodzi-Tony-Halik-i-Zbigniew-Herbert_content.jpg',
